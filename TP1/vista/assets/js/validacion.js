440ee0ab00aad6c44f7afae367b3806e7f830360
Sofia-NB <sofiabascur.80@gmail.com>
2025-08-29 17:32:41 -0300

    Sexto commit

--- TP1/vista/assets/js/validacion.js
+++ TP1/vista/assets/js/validacion.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   const formulario1 = document.getElementById("form1");
   const formulario3 = document.getElementById("form3");
+  const formulario2 = document.getElementById("form2");
+
 
 if (formulario1) {
   formulario1.addEventListener("submit", function (event) {
@@ -14,6 +16,72 @@ if (formulario1) {
       return;
     } 
     if (isNaN(numero)) {
+      event.preventDefault();
+      alert("Debes ingresar un número válido.");
+      return;
+    }
+
+    alert("Formulario enviado con éxito ✅");
+    this.submit();
+  });
+}
+
+if (formulario2) {
+  formulario2.addEventListener("submit", function (event) {
+
+    const lunes = document.getElementById("lunes").value.trim();
+    const martes = document.getElementById("martes").value.trim();
+    const miercoles = document.getElementById("miercoles").value.trim();
+    const jueves = document.getElementById("jueves").value.trim();
+    const viernes = document.getElementById("viernes").value.trim();
+
+    // Validar número
+    if (lunes === "") {
+      event.preventDefault();
+      alert("Lunes es obligatorio.");
+      return;
+    }else if(!Number.isInteger(Number(lunes)) || Number(lunes) > 24){
+      event.preventDefault();
+      alert("Debes ingresar un número válido.");
+      return;
+    }
+
+
+    if (martes === "") {
+      event.preventDefault();
+      alert("Martes es obligatorio.");
+      return;
+    }else if(!Number.isInteger(Number(martes)) || Number(martes) > 24){
+      event.preventDefault();
+      alert("Debes ingresar un número válido.");
+      return;
+    }
+
+    if (miercoles === "") {
+      event.preventDefault();
+      alert("Miercoles es obligatorio.");
+      return;
+    }else if(!Number.isInteger(Number(miercoles)) || Number(miercoles) > 24){
+      event.preventDefault();
+      alert("Debes ingresar un número válido.");
+      return;
+    }
+
+    if (jueves === "") {
+      event.preventDefault();
+      alert("Jueves es obligatorio.");
+      return;
+    }else if(!Number.isInteger(Number(jueves)) || Number(jueves) > 24){
+      event.preventDefault();
+      alert("Debes ingresar un número válido.");
+      return;
+    }
+
+    if (viernes === "") {
+      event.preventDefault();
+      alert("Viernes es obligatorio.");
+      return;
+    }else if(!Number.isInteger(Number(viernes)) || Number(viernes) > 24){
       event.preventDefault();
       alert("Debes ingresar un número válido.");
       return;
